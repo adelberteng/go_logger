5ec1a7ca43ad3e03f033198a96dd7966d2f28d19
Albert <adelberteng@gmail.com>
2022-01-25 13:42:26 +0800

    minor refine

--- go_logger.go
+++ go_logger.go
@@ -1,9 +1,9 @@
 package go_logger
 
 import (
-	"os"
-	"io"
-	"log"
+    "os"
+    "io"
+    "log"
 )
 
 const (
@@ -15,61 +15,61 @@ const (
 )
 
 func convertLevelToInt(s string) int {
-	levelMap := map[string]int{
-		"debug": 0,
-		"info": 1,
-		"warning": 2,
-		"error": 3,
-		"critical": 4,
-	}
-	levelInt := levelMap[s]
-	return levelInt
+    levelMap := map[string]int{
+        "debug": 0,
+        "info": 1,
+        "warning": 2,
+        "error": 3,
+        "critical": 4,
+    }
+    levelInt := levelMap[s]
+    return levelInt
 }
 
 type Logger struct {
-	logger *log.Logger
-	level string
+    logger *log.Logger
+    level string
 }
 
 func (l *Logger) Debug(o interface{}) {
-	levelInt := convertLevelToInt(l.level)
-	if levelInt <= LevelDebug {
-		l.logger.Println("[DEBUG] ", o)
-	}
+    levelInt := convertLevelToInt(l.level)
+    if levelInt <= LevelDebug {
+        l.logger.Println("[DEBUG] ", o)
+    }
 }
 
 func (l *Logger) Info(o interface{}) {
-	levelInt := convertLevelToInt(l.level)
-	if levelInt <= LevelInfo {
-		l.logger.Println("[INFO] ", o)
-	}
+    levelInt := convertLevelToInt(l.level)
+    if levelInt <= LevelInfo {
+        l.logger.Println("[INFO] ", o)
+    }
 }
 
 func (l *Logger) Warning(o interface{}) {
-	levelInt := convertLevelToInt(l.level)
-	if levelInt <= LevelWarning {
-		l.logger.Println("[WARNING] ", o)
-	}
+    levelInt := convertLevelToInt(l.level)
+    if levelInt <= LevelWarning {
+        l.logger.Println("[WARNING] ", o)
+    }
 }
 
 func (l *Logger) Error(o interface{}) {
-	levelInt := convertLevelToInt(l.level)
-	if levelInt <= LevelError {
-		l.logger.Println("[ERROR] ", o)
-	}
+    levelInt := convertLevelToInt(l.level)
+    if levelInt <= LevelError {
+        l.logger.Println("[ERROR] ", o)
+    }
 }
 
 func (l *Logger) Critical(o interface{}) {
-	levelInt := convertLevelToInt(l.level)
-	if levelInt <= LevelCritical {
-		l.logger.Println("[CRITICAL] ", o)
-	}
+    levelInt := convertLevelToInt(l.level)
+    if levelInt <= LevelCritical {
+        l.logger.Println("[CRITICAL] ", o)
+    }
 }
 
-func CreateLogger(f *os.File, level string) Logger {
-	writers := []io.Writer{f, os.Stdout}
-	logger := log.New(io.MultiWriter(writers...), os.Args[0], log.Ldate|log.Ltime)
-	newLogger := Logger{logger: logger, level: level}
+func CreateLogger(f *os.File, level string, logSrc string) Logger {
+    writers := []io.Writer{f, os.Stdout}
+    logger := log.New(io.MultiWriter(writers...), logSrc, log.Ldate|log.Ltime)
+    newLogger := Logger{logger: logger, level: level}
 
-	return newLogger
+    return newLogger
 }
